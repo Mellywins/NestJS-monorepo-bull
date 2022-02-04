fce551ca2766d87d562ded532374b4d62d8c1a5e
Mellywins <zouaghi.wow1919@gmail.com>
2022-02-04 14:23:54 +0100

    bull setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,29 @@
+import { BullModule } from '@nestjs/bull';
 import { Module } from '@nestjs/common';
+import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    BullModule.forRoot({
+      redis:{
+        host:'localhost',
+        port:6379
+      }
+    }),
+    BullModule.registerQueue({
+      name: 'queue1',
+      processors: [
+        {
+          path: join(
+            __dirname,
+            'processor/test.processor.js',
+          ),
+        },
+      ],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/processor/test.processor.ts
@@ -0,0 +1,7 @@
+
+export default function (job,cb) {
+  const {sourcePath, outputPath} = job.data;
+  console.log(
+    `[${process.pid}] Attempting Compression delegated to job with UUID`,
+    );
+}
